6bc04e6675ceb3bd920b2d2f8cb9d794040975d2
Matthias Nötzli <matthias.noetzli@gmail.com>
2021-11-10 15:15:10 +0100

    Display either logo or title and description

--- header.php
+++ header.php
@@ -30,30 +30,22 @@
 			<div class="site-header-wrapper">
 				<div class="site-branding">
 					<?php
-					the_custom_logo();
-					?>
-
-					<!-- 				
-					<?php 
-					if ( is_front_page() && is_home() ) :
+					if ( has_custom_logo() ) :
+						the_custom_logo();
+					else :                     
 						?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php
-											else :
-												?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>						
 												<?php
-											endif;
-											$hfh_theme_description = get_bloginfo( 'description', 'display' );
-											if ( $hfh_theme_description || is_customize_preview() ) :
-												?>
+												$hfh_theme_description = get_bloginfo( 'description', 'display' );
+												if ( $hfh_theme_description || is_customize_preview() ) :
+													?>
 					<p class="site-description">
-												<?php 
-												echo $hfh_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-												?>
+													<?php 
+													echo $hfh_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+													?>
 												</p>
-				<?php endif; ?> -->
-
+					<?php endif; ?> 
+					<?php endif; ?>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation">
