8de51695c08a87f031236cc400a86eb45db712b8
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-03-09 08:36:43 +0100

    Update content templates

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,13 +1,17 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('hfh-content hfh-theme-content'); ?>>
     <header>
         <?php
-        the_title('<h1>', '</h1>');
+        if (is_front_page()) {
+            the_title('<h1 class="hfh-sr-only">', '</h1>');
+        } else {
+            the_title('<h1>', '</h1>');
+        }
         ?>
     </header>
     <?php if (has_post_thumbnail()) : ?>
         <div class="hfh-article__image hfh-header-image"><?php the_post_thumbnail() ?></div>
     <?php endif; ?>
-    <div class="hfh-article__content">
-        <?php the_content() ?>
-    </div>
+
+    <?php the_content() ?>
+
 </article>
