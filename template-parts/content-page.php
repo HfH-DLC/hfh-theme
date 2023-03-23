927934515bcdfe0fbde3157fa454bb599ef6ce69
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-03-23 13:48:53 +0100

    Update content-page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,11 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('hfh-content hfh-theme-content'); ?>>
     <header>
         <?php
-        if (is_front_page()) {
-            the_title('<h1 class="hfh-sr-only">', '</h1>');
-        } else {
-            the_title('<h1>', '</h1>');
-        }
+        the_title('<h1>', '</h1>');
         ?>
     </header>
     <?php if (has_post_thumbnail()) : ?>
@@ -13,5 +9,4 @@
     <?php endif; ?>
 
     <?php the_content() ?>
-
 </article>
