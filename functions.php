c28bab6b614a0435e3b5ef570a9ae74a1a0e948f
Matthias Nötzli <matthias.noetzli@gmail.com>
2024-01-30 16:19:53 +0100

    Version 2.0.22

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 
 if (!defined('HFH_THEME_VERSION')) {
     // Replace the version number of the theme on each release.
-    define('HFH_THEME_VERSION', '2.0.21');
+    define('HFH_THEME_VERSION', '2.0.22');
 }
 
 require(get_template_directory() . '/inc/hfh_theme_category_filter.php');
